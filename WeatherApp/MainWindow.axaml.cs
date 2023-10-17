3ced1e53c08f88dea05e5ad237d772a3b6e7f26b
plukas <lukas.portier@ynov.com>
2023-10-17 16:07:26 +0200

    Affichage template

--- WeatherApp/MainWindow.axaml.cs
+++ WeatherApp/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace WeatherApp;
 
@@ -7,5 +9,26 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ValiderRecherche(object? sender, RoutedEventArgs e)
+    {
+        Button b = sender as Button;
+        string search = SearchBar.Text.ToString();
+        if (search == "")
+        {
+            Error.Content = "Entrez le nom d'une ville avant de valider";
+        }
+        else
+        {
+            SearchBar.Text = "";
+            Error.Content = "";
+            Ville.Content = search;
+            Coord.Content = "Coordonées";
+            Image.Content = "Image";
+            Temp.Content = "Température";
+            Desc.Content = "Description";
+            Hum.Content = "Humidité";
+        }
     }
 }
