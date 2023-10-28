7f64d37feb9e3f789711989e994f91e0f40aa5e7
plukas <lukas.portier@ynov.com>
2023-10-28 13:50:48 +0200

    Prevision template

--- WeatherApp/MainWindow.axaml.cs
+++ WeatherApp/MainWindow.axaml.cs
@@ -31,4 +31,41 @@ public partial class MainWindow : Window
             Hum.Content = "Humidité";
         }
     }
+    
+    private void ValiderRecherche2(object? sender, RoutedEventArgs e)
+    {
+        Button b = sender as Button;
+        string search = SearchBar2.Text.ToString();
+        if (search == "")
+        {
+            Error2.Content = "Entrez le nom d'une ville avant de valider";
+        }
+        else
+        {
+            SearchBar2.Text = "";
+            Error2.Content = "";
+            Ville2.Content = search;
+            Coord2.Content = "Coordonées";
+            Image2.Content = "Image";
+            Temp2.Content = "Température";
+            Desc2.Content = "Description";
+            Hum2.Content = "Humidité";
+            Image3.Content = "Image";
+            Temp3.Content = "Température";
+            Desc3.Content = "Description";
+            Hum3.Content = "Humidité";
+            Image4.Content = "Image";
+            Temp4.Content = "Température";
+            Desc4.Content = "Description";
+            Hum4.Content = "Humidité";
+            Image5.Content = "Image";
+            Temp5.Content = "Température";
+            Desc5.Content = "Description";
+            Hum5.Content = "Humidité";
+            Image6.Content = "Image";
+            Temp6.Content = "Température";
+            Desc6.Content = "Description";
+            Hum6.Content = "Humidité";
+        }
+    }
 }
